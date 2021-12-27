6c0aefb8a34e284c00a9fdd32100700096095b40
Liderador99 <kevin.c.jesus@outlook.com>
2021-12-27 18:10:28 -0300

    Classe Reservation completa

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,5 +1,13 @@
 package Application;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+	}
 }
--- src/Models/Entitites/Reservation.java
+++ src/Models/Entitites/Reservation.java
@@ -1,5 +1,58 @@
 package Models.Entitites;
 
-public class Reservation {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+public class Reservation {
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkOut;
+	
+	public Reservation(Integer roomNumber, Date checkin, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkin.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // conversão de milisegungos para dias
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkin = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", Check-in: "
+				+ sdf.format(checkin)
+				+ ", Check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nihgts";
+	}
 }
